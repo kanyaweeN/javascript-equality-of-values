0b51c9d452875034f92b1ca793b78dbd413b3ab2
kanyaweeN <40629441+kanyaweeN@users.noreply.github.com>
2025-07-18 14:40:26 +0700

    feat:edit ex1

--- ex-1.js
+++ ex-1.js
@@ -1,20 +1,21 @@
 // Exercise #1: Strict and Same
 // Start commenting here
-Object.is(10, 20);
-Object.is(0, -0);
-Object.is(NaN, 100);
-Object.is("one", "two");
-Object.is({ name: "John", age: 30 }, { name: "John", age: 30 });
-Object.is(null, undefined);
+Object.is(10, 20); //false 
+Object.is(0, -0); //false 
+Object.is(NaN, 100); //false 
+Object.is("one", "two"); //false 
+Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); //false 
+Object.is(null, undefined); //false 
 
-10 === 20; //Expression 
-10 === "10" //Expression 
-3.14 !== 3.14; //Expression 
-"TechUp" !== "TechUp"; //Expression 
-NaN === NaN; //Expression 
-0 !== -0; //Expression 
+10 === 20; //false 
+10 === "10" //false 
+3.14 !== 3.14; //false 
+"TechUp" !== "TechUp"; //false 
+NaN === NaN; //false 
+0 !== -0; //false 
 
 let objectA = { course: "Web Developer", price: 300000 };
 let objectB = { course: "Web Developer", price: 300000 };
-objectA === objectB;  //Expression 
+objectA === objectB;  //false 
 
+console.log(Object.is({ name: "John", age: 30 }, { name: "John", age: 30 })); //false
