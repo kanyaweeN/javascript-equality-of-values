03914b1fc7f65f546a9a2c710f90230e3f168a0a
lalinyuuu <lullalin.yu@gmail.com>
2025-07-18 13:15:47 +0700

    refactor: add comments  ex 1-2

--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,8 @@
 // Exercise #2: Loose Equality
 // Start commenting here
-0 == false;
-null == undefined;
-10 == "10";
-[10] == 10;
-[1,2] == "1,2";
+0 == false; //true เพราะ f แปลงเป็น 0 ก่อนจะเทียบมั้ง
+null == undefined; //true 
+10 == "10"; //true  สติงแปลงเป็นนัมเบอ
+[10] == 10; //true *  array > แปลง string auto > "" แปลง number
+[1,2] == "1,2"; //true สติงเท่ากัน
+
